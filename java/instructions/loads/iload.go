b2755d74d23f756105c10e199f0e4061c46c72b6
sun <sixsixsix516@163.com>
2020-02-18 15:17:39 +0800

    定义指令集2

---
+++ java/instructions/loads/iload.go
@@ -0,0 +1,20 @@
+package loads
+
+import "../../instructions/base"
+import "../../rtda"
+
+// Load int from local variable
+type ILOAD struct{ base.Index8Instruction }
+type ILOAD_0 struct{ base.NoOperandsInstruction }
+type ILOAD_1 struct{ base.NoOperandsInstruction }
+type ILOAD_2 struct{ base.NoOperandsInstruction }
+type ILOAD_3 struct{ base.NoOperandsInstruction }
+
+func _iload(frame *rtda.Frame,index uint){
+	val := frame.LocalVars().GetInt(index)
+	frame.OperandStack().PushInt(val)
+}
+
+func (self *ILOAD) Execute(frame *rtda.Frame){
+	_iload(frame,uint(self.Index))
+}
